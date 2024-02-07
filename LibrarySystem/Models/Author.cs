f7768ee2dc612c6eee19bf5f3ece0223f402084c
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-07 09:31:16 -0800

    Add Author

---
+++ LibrarySystem/Models/Author.cs
@@ -0,0 +1,9 @@
+namespace LibrarySystem.Models
+{
+  public class Author
+  {
+    public int AuthorId { get; set; }
+    public string AuthorFirstName { get; set; }
+    public string AuthorLastName { get; set; }
+  }
+}
--- LibrarySystem/Models/LibrarySystemContext.cs
+++ LibrarySystem/Models/LibrarySystemContext.cs
@@ -4,7 +4,7 @@ namespace LibrarySystem.Models
 {
   public class LibrarySystemContext : DbContext
   {
-    // public DbSet<Student> Students { get; set; }
+    public DbSet<Author> Authors { get; set; }
     public LibrarySystemContext(DbContextOptions options) : base(options) { }
   }
 }
